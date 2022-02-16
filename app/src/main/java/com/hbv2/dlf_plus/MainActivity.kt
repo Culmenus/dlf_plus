0ba18c3402dbd11e32eeacd4ee7ab447a1e8880d
EatCakeAndDie <61690317+EatCakeAndDie@users.noreply.github.com>
2022-02-16 21:47:24 +0000

    first commit

---
+++ app/src/main/java/com/hbv2/dlf_plus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hbv2.dlf_plus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
