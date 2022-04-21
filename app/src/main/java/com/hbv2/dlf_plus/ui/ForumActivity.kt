bb5017529ecf38e3b1a2df096f1f3d66839f9cda
EatCakeAndDie <61690317+EatCakeAndDie@users.noreply.github.com>
2022-04-21 20:51:38 +0000

    Oh bottomNav boi, the pipes the pipes are calling

--- app/src/main/java/com/hbv2/dlf_plus/ui/ForumActivity.kt
+++ app/src/main/java/com/hbv2/dlf_plus/ui/ForumActivity.kt
@@ -84,23 +84,6 @@ class ForumActivity : AppCompatActivity(), OnTopicCreated {
             createTopic.show(supportFragmentManager, "createTopic")
         }
 
-        binding.bottomNavigation.setOnItemReselectedListener { item ->
-            when (item.itemId) {
-                R.id.botItem1 -> {
-                    // Respond to navigation item 1 click
-                    true
-                }
-                R.id.botItem2 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                R.id.botItem3 -> {
-                    // Respond to navigation item 2 click
-                    true
-                }
-                else -> false
-            }
-        }
 
         binding.cover.setImageResource(forum.cover)
         binding.name.text = forum.name
